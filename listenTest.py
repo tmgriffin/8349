fec5cbfb250fe1b2956f86492f2522966fc3b725
tmgriffin <tmgriffin@smu.edu>
2015-02-22 18:18:40 -0600

    Added comments

--- listenTest.py
+++ listenTest.py
@@ -4,25 +4,26 @@ import pyaudio,struct
 from aubio.task import *
 
 
-CHUNK = 1024
+# Defines constants for the audio components. Don't change probably
+CHUNK = 1024 
 FORMAT = pyaudio.paFloat32
 CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = 3
-WAVE_OUTPUT_FILENAME = "output.wav"
 PITCHALG    = aubio_pitch_yin
 PITCHOUT    = aubio_pitchm_freq
 
 
+#Open listening stream
 p = pyaudio.PyAudio()
-
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
                 
-                # set up pitch detect
+
+# set up pitch detect
 detect = new_aubio_pitchdetection(CHUNK,CHUNK/2,CHANNELS,
                                   RATE,PITCHALG,PITCHOUT)
 buf = new_fvec(CHUNK,CHANNELS)
@@ -31,14 +32,12 @@ print("* recording")
 
 frames = []
 
+#Record audio for given time
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
-    
     floats = struct.unpack('f'*(len(data)/4),data)
-    
     #print floats
     frames.append(floats)
-
 print("* done recording")
 
 #print frames
@@ -47,7 +46,10 @@ stream.close()
 p.terminate()
 
 #exit()
+
 for j in range(len(frames)):
+	
+	#Write the sample's values into the required data structure
 	for i in range(len(frames[j])):
 		fvec_write_sample(buf, frames[j][i], 0, i)
  
@@ -60,12 +62,3 @@ for j in range(len(frames)):
 	print "\n\n\n\nHEY LISTEN \n{:10.4f} {:10.4f}".format(freq,energy)
 	#del_fvec(buf)
 	#buf = new_fvec(CHUNK,CHANNELS)
-
-"""
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
-"""
